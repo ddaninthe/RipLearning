430ed515da78d679fdbaaa5d5e2b83e6da868ae5
Nico C <nicococq25@gmail.com>
2020-02-18 14:06:39 +0100

    Update Script + Add DLL

--- Vidality/Assets/RetrieveAndModifySpherePositionsScript.cs
+++ Vidality/Assets/RetrieveAndModifySpherePositionsScript.cs
@@ -7,16 +7,22 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
 {
 
     [DllImport("ViDLL.dll")]
-    private static extern IntPtr createModel(int nbInputs);
+    private static extern IntPtr createLinearModel(int nbInputs);
 
     [DllImport("ViDLL.dll")]
-    private static extern double predictLinear(IntPtr model, int size, double[] inputs);
+    private static extern IntPtr trainLinearClassification(double[] dataset, int dataSize, IntPtr model, int modelSize, double iterNumber, double learning);
+
+    [DllImport("ViDLL.dll")]
+    private static extern int predictLinearClassification(IntPtr model, int size, double[] inputs);
+
+    [DllImport("ViDLL.dll")]
+    private static extern void clear(IntPtr model);
 
     public Transform[] trainingSpheres;
     public Transform[] testSpheres;
 
     private double[] trainingInputs;
-    private double[] trainingExpectedOutputs;
+    //private double[] trainingExpectedOutputs;
 
     private IntPtr model;
 
@@ -33,21 +39,35 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
     
     public void CreateModel()
     {
-        model = createModel(2);
+
+        ReleaseModel();
+        model = createLinearModel(2);
+        PredictOnTestSpheres();
     }
 
     public void Train()
     {
-        trainingInputs = new double[trainingSpheres.Length * 2];
-        trainingExpectedOutputs = new double[trainingSpheres.Length];
+        trainingInputs = new double[trainingSpheres.Length * 3];
+        //trainingExpectedOutputs = new double[trainingSpheres.Length];
 
         for (var i = 0; i < trainingSpheres.Length; i++)
         {
-            trainingInputs[2 * i] = trainingSpheres[i].position.x;
-            trainingInputs[2 * i + 1] = trainingSpheres[i].position.z;
-            trainingExpectedOutputs[i] = trainingSpheres[i].position.y;
+            trainingInputs[3 * i] = trainingSpheres[i].position.x;
+            trainingInputs[3 * i + 1] = trainingSpheres[i].position.z;
+            trainingInputs[3 * i + 2] = trainingSpheres[i].position.y;
         }
-        
+
+        trainLinearClassification(trainingInputs, trainingInputs.Length, model, 2, 1000, 0.02);
+
+        ///Test post training
+        /*for (var i = 0; i < trainingSpheres.Length; i++)
+        {
+            Debug.Log("training Sphere numero " + i + " : x = " + trainingSpheres[i].position.x);
+            Debug.Log("training Sphere numero " + i + " : z = " + trainingSpheres[i].position.z);
+            Debug.Log("training Sphere numero " + i + " : y = " + trainingSpheres[i].position.y);
+        }*/
+
+
         // TrainLinearModelRosenblatt(model, trainingInputs, 2, trainingSpheres.Length, trainingExpectedOutputs, 1, 0.01, 1000)
     }
 
@@ -56,7 +76,7 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
         for (var i = 0; i < testSpheres.Length; i++)
         {
             var input = new double[] {testSpheres[i].position.x, testSpheres[i].position.z};
-            var predictedY = predictLinear(model, 2, input);
+            var predictedY = predictLinearClassification(model, 2, input);
             //var predictedY = Random.Range(-5, 5);
             testSpheres[i].position = new Vector3(
                 testSpheres[i].position.x,
@@ -67,6 +87,11 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
 
     public void ReleaseModel()
     {
-        // FreeLinearModel(model);
+        if (model != null)
+        {
+            clear(model);
+            ReInitialize();
+            model = IntPtr.Zero;
+        }
     }
 }
