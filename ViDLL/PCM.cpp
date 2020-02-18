b83977d212ab914df30e174e89572ef7a68aaef1
Nicolas OTTON <otton.nicolas@orange.fr>
2020-02-18 14:24:03 +0100

    added cell create

--- ViDLL/PCM.cpp
+++ ViDLL/PCM.cpp
@@ -1,20 +1,28 @@
 #include "PCM.h"
 #include "pch.h"
 
-double* createPCMModel(int* layout, int size) {
+double* createPCMModel(int* layout, int size, int cellSize) {
 	double* model = new double[size];
 	for (int i = 0; i < size; i++) {
-		model[i] = createCellArray(layout[i])
+		model[i] = createCellArray(layout[i], cellSize);
 	}
 	return model;
 }
 
-double* createCellArray(int cellNumber) {
+double* createCellArray(int cellNumber, int cellSize) {
 	double* cellArray = new double[cellNumber];
 	for (int j = 0; j < cellNumber; j++) {
-		cellArray[j] = ((rand() / (double)RAND_MAX) - 0.5) * 2;
+		cellArray[j] = createCell(int cellSize);
 	}
-	return cellArray
+	return cellArray;
+}
+
+double* createCell(int cellSize) {
+	double* cell = new double[cellSize];
+	for (int k = 0; k < cellSize; j++) {
+		cell[j] = ((rand() / (double)RAND_MAX) - 0.5) * 2;
+	}
+	return cell;
 }
 
 void trainPCMClassification() {
