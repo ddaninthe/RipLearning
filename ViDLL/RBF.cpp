f3773e91cb014b24fd0d518a122a68c93f80c3a1
ddaninthe <d.daninthelopez@gmail.com>
2020-02-19 12:30:28 +0100

    RBF Predict and Creation Done

--- ViDLL/RBF.cpp
+++ ViDLL/RBF.cpp
@@ -2,6 +2,41 @@
 #include "pch.h"
 
 
-double* createRBFModel(double* dataset, int datasetSize, int dimensions) {
-	return (double*)1;
+RBF* createRBFModel(double* dataset, int datasetSize, int dimensions) {
+	RBF* model = new RBF[datasetSize];
+	for (int i = 0; i < datasetSize; i++) {
+		double* coor = new double[dimensions];
+		for (int c = 0; c < dimensions; c++) {
+			coor[c] = dataset[dimensions * i + c];
+		}
+		model[i].coordinates = coor;
+		model[i].weight = ((rand() / (double) RAND_MAX) - 0.5) * 2;
+	}
+
+	return model;
+}
+
+void trainNaiveRBF() {
+
+}
+
+int predictRBFClassification(RBF* model, int gamma, double* inputs, int dimensions, int modelSize) {
+	return predictRBFRegression(model, gamma, inputs, dimensions, modelSize) >= 0 ? 1 : -1;
+}
+
+double predictRBFRegression(RBF* model, int gamma, double* inputs, int dimensions, int modelSize) {
+	double res = 0;
+	for (int i = 0; i < modelSize; i++) {
+		double exponent = -gamma * squareMagnitude(inputs, model[i].coordinates, dimensions);
+		res += model[i].weight * exp(exponent);
+	}
+	return res;
+}
+
+double squareMagnitude(double* a, double* b, int dim) {
+	double magn = 0;
+	for (int i = 0; i < dim; i++) {
+		magn += pow(a[i] - b[i], 2);
+	}
+	return magn;
 }
--- ViDLL/RBF.h
+++ ViDLL/RBF.h
@@ -1,11 +1,14 @@
 #pragma once
 
-struct RBF {
-	double weight;
-	double* coordinates;
-	int dimensions;
-};
-
 extern "C" {
-	__declspec(dllexport) double* createRBFModel(double* dataset, int datasetSize, int dimensions);
+	struct RBF {
+		double weight;
+		double* coordinates;
+	};
+
+	__declspec(dllexport) RBF* createRBFModel(double* dataset, int datasetSize, int dimensions);
+	__declspec(dllexport) int predictRBFClassification(RBF* model, int gamma, double* inputs, int dimensions, int modelSize);
+	__declspec(dllexport) double predictRBFRegression(RBF* model, int gamma, double* inputs, int dimensions, int modelSize);
 }
+
+double squareMagnitude(double* a, double* b, int dim);
